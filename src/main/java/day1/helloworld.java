21f5a44fe48d8f1dc1fbfab54b1f13025e2efcb6
ravitejapothala <raviteja.pothala.753@my.csun.edu>
2023-06-22 12:56:12 -0700

    firstdemoedit

--- src/main/java/day1/helloworld.java
+++ src/main/java/day1/helloworld.java
@@ -4,6 +4,6 @@ import java.sql.SQLOutput;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("ello");
+        System.out.println("hello");
     }
 }
